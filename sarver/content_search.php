021f59e8c45bac5e7ab87da420b96d82cafcf67b
Tarou Yamada <tora3546@yahoo.co.jp>
2012-09-21 17:46:08 +0900

    phpのクローラです．

--- sarver/content_search.php
+++ sarver/content_search.php
@@ -1,11 +1,16 @@
-﻿<?php
-	mb_internal_encoding('UTF-8');
-	  $file = @file("list.html"); 
-		while(list($key,$value) = each($file)){
-		print "$value";
-		}
-		//print mb_internal_encoding();
-     
+<?php
 
+$ch = curl_init("http://higashihiroshima.genki365.net/gnkh12/pub/calendar.php");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
+$output = curl_exec($ch); //実行
+curl_close($ch); //cURL セッションを閉じる
 
-?>
+$replace_output = str_replace("sheet.php", "http://higashihiroshima.genki365.net/gnkh12/pub/sheet.php", $output);
+
+print $replace_output; //確認用
+
+$fp = fopen('list.html', 'w');
+fwrite($fp, $replace_output);
+fclose($fp);
+
+?>  
