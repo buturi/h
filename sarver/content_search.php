cef3d23e3adc9e7ea514c0d87a1170bb8647a866
Tarou Yamada <tora3546@yahoo.co.jp>
2012-09-23 19:47:09 +0900

    testをlistに変更

--- sarver/content_search.php
+++ sarver/content_search.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$html_time = date("d", filemtime("test.html"));
-$today = date("d");
+$html_time = date("d", filemtime("list.html")); //list.htmlの更新日時を取得
+$today = date("d"); //日にち取得
 
-if ( $html_time != $today or !file_exists("test.html") ) {
+if ( $html_time != $today or !file_exists("list.html") ) {
 
 $ch = curl_init("http://higashihiroshima.genki365.net/gnkh12/pub/calendar.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
@@ -15,7 +15,7 @@ $replace_output = str_replace("sheet.php", "http://higashihiroshima.genki365.net
 
 print $replace_output; //確認用
 
-$fp = fopen('test.html', 'w');
+$fp = fopen('list.html', 'w');
 fwrite($fp, $replace_output);
 fclose($fp);
 
