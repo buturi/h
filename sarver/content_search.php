cbb41dfb3e171781272d1156f66ea08bb0975072
Tarou Yamada <tora3546@yahoo.co.jp>
2012-10-05 19:41:28 +0900

    真�

--- sarver/content_search.php
+++ sarver/content_search.php
@@ -1,24 +1,38 @@
 <?php
+/* $targetURLのファイルを取得し，保存する. */
+
+$localFile = "list.html";
+$localURL = "http://buturi.heteml.jp/student/higashihiroshima/";
+$targetURL = "http://higashihiroshima.genki365.net/gnkh12/pub/content_search.php?type=11&sort=5&select_class=1&select_code1=1";
 
 //ファイルが存在しているかどうか
-if ( file_exists("list.html") ) {
-$html_time = date("d", filemtime("list.html")); //list.htmlの更新日時を取得
-$today = date("d"); //日にち取得
+if ( file_exists($localFile) ) {
+	$html_time = date("d", filemtime($localFile)); //list.htmlの更新日時を取得
+	$today = date("d"); //日にち取得
 }
 
 //今日の日にちがファイルの更新日と異なる　または　ファイルが存在しない
-if ( !file_exists("list.html") or $html_time != $today ) {
-mb_internal_encoding('UTF-8'); //エンコーディング
-$url = "http://higashihiroshima.genki365.net/gnkh12/pub/calendar.php";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
-$output = curl_exec($ch); //実行
-curl_close($ch); //cURL セッションを閉じる
-
-$fp = fopen('list.html', 'w'); //ファイルを開く
-flock($fp , LOCK_EX ); //ファイルをロック
-fwrite($fp, $output); //書き込み
-fclose($fp); //閉じる
+if ( !file_exists($localFile) or $html_time != $today ) {
+	$ch = curl_init($targetURL);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
+	$result = curl_exec($ch); //実行
+	curl_close($ch); //cURL セッションを閉じる
+	
+	$fp = fopen($localFile, 'w'); //ファイルを開く
+	flock($fp , LOCK_EX ); //ファイルをロック
+	$result = str_replace("EUC-JP","UTF-8",$result); // 置換
+	$result = mb_convert_encoding($result, "UTF-8", "EUC-JP"); //list.htmlをUTF-8に変換
+	fwrite($fp, $result); //書き込み
+	fclose($fp); //閉じる
 }
+else {
+	$targetURL = $localURL.$localFile;
+	$ch = curl_init($targetURL);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
+	$result = curl_exec($ch); //実行
+	curl_close($ch); //cURL セッションを閉じる
+}
+
+echo $result; //表示
 
 ?>  
--- sarver/sheet.php
+++ sarver/sheet.php
@@ -1,36 +1,38 @@
 <?php
+/* targetURLのファイルを取得し，保存する． */
 
-if( file_exists( $_GET['id'] .".html" ) ){
-	$html_time = date( "d",filemtime( $_GET['id'] .".html" ) ); //htmlファイルの
+$fileName = "page/".$_GET['id'].".html";
+$localURL = "http://buturi.heteml.jp/student/higashihiroshima/";
+$targetURL = "http://higashihiroshima.genki365.net/gnkh12/pub/sheet.php?id=".$_GET['id'];
+
+if(file_exists($fileName)){
+	$html_time = date("d",filemtime($fileName)); //htmlファイルの更新日時取得
 	$today = date("d"); //日にち取得
 
 }
 
-//今日の日にちがファイルの更新日と異なる　または　ファイルが存在しない
-if ( !file_exists( ( "list.html") or $html_time != $today ) and $_GET['id'] ) {
-	mb_internal_encoding('UTF-8'); //エンコーディング
-	$url =  "http://higashihiroshima.genki365.net/gnkh12/pub/sheet.php?id=". $GET['id'];
-	$ch = curl_init($url);
+//(今日の日にちがファイルの更新日と異なる　または　ファイルが存在しない)
+if (!file_exists(($fileName) or $html_time != $today)) {
+	$ch = curl_init($targetURL);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
-	$output = curl_exec($ch); //実行
+	$result = curl_exec($ch); //実行
 	curl_close($ch); //cURL セッションを閉じる
-
-	$fileName = $_GET['id'].".html";
-	$fp = fopen( '$fileName', 'w' ); //ファイルを開く
+	
+	$fp = fopen($fileName, 'w'); //ファイルを開く
 	flock($fp , LOCK_EX ); //ファイルをロック
-	fwrite($fp, $replace_output); //書き込み
+	$result = str_replace("EUC-JP","UTF-8",$result); // 置換
+	$result = mb_convert_encoding($result, "UTF-8", "EUC-JP"); //ファイルをUTF-8に変換
+	fwrite($fp, $result); //書き込み
 	fclose($fp); //閉じる
 }
+else {
+	$targetURL = $localURL.$fileName;
+	$ch = curl_init($targetURL);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); //Trueの場合，curl_exec()の返り値を文字列で返す
+	$result = curl_exec($ch); //実行
+	curl_close($ch); //cURL セッションを閉じる
+}
 
-
-
-
-
-
-
-
-
-
-
+echo $result; //表示
 
 ?>
