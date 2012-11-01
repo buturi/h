2301c895189d6ee1b3a4f1ab195c638631e444bd
Tarou Yamada <tora3546@yahoo.co.jp>
2012-11-01 23:54:11 +0900

    並び替えて表示　完成！

--- client/js/scrape.js
+++ client/js/scrape.js
@@ -18,6 +18,7 @@
 
 var eventData={"event":new Array()};//グローバル変数､eventDataを定義､最初にイベント群を保持するArrayを定義する
 var j=0;
+var execution=0;
 
 /*デバッグ用サイト1*/
 //var domain="http://www7b.biglobe.ne.jp/~pso/tmp/";
@@ -307,18 +308,26 @@ var domain="http://buturi.heteml.jp/student/higashihiroshima/";
 				/*eventDataに一時的に作っていたObjectを入れる*/
 				eventData.event.unshift(tmpEventData);
 				var b = eventData.event.length-1;
-				for (var a=0;a<b;a++) {
-					/*alert(eventData.event[a+1]["id"]+" - "+eventData.event[a]["id"]);*/
-					if (eventData.event[a+1]["id"] < eventData.event[a]["id"]) {
-						var tmp = eventData.event[a];
-						eventData.event[a] = eventData.event[a+1];
-						eventData.event[a+1] = tmp;
-					}else {
-						break;
+
+				/*if (b==0){ //最初に取得したイベントを表示
+					$("#box").append("<div class='eventBox' onClick='moveMap("+j+")'>"+eventData.event[0]['id']+"</div>");
+				}*/
+
+				L: for (a=0;a<=b;a++) { // id順に並び替え
+					if (a<b) {
+						if (eventData.event[a+1]["id"] < eventData.event[a]["id"]) {
+							var tmp = eventData.event[a];
+							eventData.event[a] = eventData.event[a+1];
+							eventData.event[a+1] = tmp;
+						} else{
+						$(".eventBox:eq("+(a)+")").before("<div class='eventBox' onClick='moveMap("+j+")'>"+eventData.event[a]['id']+"</div>");
+						break L;
+						}
+					} else{
+						$("#box").append("<div class='eventBox' onClick='moveMap("+j+")'>"+eventData.event[a]['id']+"</div>");
 					}
 				}
-
-				$("#box").append("<div class='eventBox' onClick='moveMap("+j+")'>"+eventData.event[j]['id']+"</div>");
+				
 				j++;
 			});
 			
