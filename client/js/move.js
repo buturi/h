77de082da7f3d0f8f86ad8688139558a21fc3ab2
Tarou Yamada <tora3546@yahoo.co.jp>
2012-11-14 19:10:11 +0900

    タブ追加&panToに変更

--- client/js/move.js
+++ client/js/move.js
@@ -1,10 +1,4 @@
 // JavaScript Document
 function moveMap(x) {
-	var lat_lng =new google.maps.LatLng(eventData.event[x]['latLng']['lat'],eventData.event[x]['latLng']['lng']);
-	var opts = {
-		zoom: 16,
-		center: lat_lng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("mapCanvas"), opts);
+	mapCanvas.panTo(new google.maps.LatLng(eventData.event[x]['latLng']['lat'],eventData.event[x]['latLng']['lng']));
 }
