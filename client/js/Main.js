9176a426caaa247c683edad93f8f03dc0b78263b
Yuki <tora3546@yahoo.co.jp>
2012-12-14 02:45:53 +0900

    dateソート作成途中

--- client/js/Main.js
+++ client/js/Main.js
@@ -13,7 +13,7 @@ $(function() {
 	var sideList=new SideList();
 
 	//イベント管理オブジェクトへソート関数､コールバック関数､公式リストIDを渡してロード｡
-	eventData.load(SortByID,
+	eventData.load(SortByDate,
 		function(eventObject,position){
 			//コールバック関数内部処理
 			//リストへデータを挿入する
@@ -24,5 +24,21 @@ $(function() {
 	//TODO sort実装
 	function SortByID(a,b){
 		return a.id - b.id;
+	}
+
+	function SortByDate(a,b) {
+		if ( a.date[0] && b.date[0] ) { //aとbの日付が存在する場合
+			if ( a.date[0]['from'].getTime()>b.date[0]['from'].getTime() ) {
+				return 1;
+			} else if ( a.date[0]['from'].getTime()<b.date[0]['from'].getTime() ) {
+				return -1;
+			} else {
+				return 0;
+			}
+		} else if ( a.date[0] )
+			return -1;
+		else {
+			return -1;
+		}
 	}
 });
