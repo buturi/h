0f3def01bda57d91d45bb5dafd3f3289f23f4550
情報物理研究室 <buturi>
2012-12-25 20:17:46 +0900

    ピン改造

--- client/js/GMap.js
+++ client/js/GMap.js
@@ -43,11 +43,12 @@ var GMap=(function(){
 			var gLatlng = new google.maps.LatLng(latLng.lat,latLng.lng);
 
 
-			var infoWnd = new google.maps.InfoWindow({content:"<h1>"+eventObject.title+"</h1><p>"+eventObject.time+"</p>"});
+			var infoWnd = new google.maps.InfoWindow({content:"<h1><a href='http://higashihiroshima.genki365.net/gnkh12/pub/sheet.php?id="+eventObject.id+"'>"+eventObject.title+"</a></h1><p>"+eventObject.time+"</p>"});
 
 			var marker = new google.maps.Marker({
 			    position: gLatlng, 
 			    map: _mapCanvas, 
+			    icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=O|AAFFAA|000000',
 			    title:eventObject.title
 			});
 
