f3f693d75b4d75e35b475817943a0e7c73b80e5c
wizard-paso <paso>
2013-02-10 19:54:31 +0900

    不具合を誘発してるかもしれないSideListアニメーションを消した

--- client/js/SideList.js
+++ client/js/SideList.js
@@ -59,7 +59,7 @@ var SideList=(function(){
 			colorString+="0.9);"
 			console.log(colorString);
 
-			$(sph).after("<div class='wrap'><div class='dateBox "+weeks+"'>"+days+"<div id='list"+_size+"' class='eventBox'>"+str_title+"<div class='sponsorBox' style='border-bottom: 3px solid "+colorString+"'>"+str_sponsor+"</div></div></div>").hide().show("fast");
+			$(sph).after("<div class='wrap'><div class='dateBox "+weeks+"'>"+days+"<div id='list"+_size+"' class='eventBox'>"+str_title+"<div class='sponsorBox' style='border-bottom: 3px solid "+colorString+"'>"+str_sponsor+"</div></div></div>");
 			
 
 			//クリックイベントリスナーを追加しておく
